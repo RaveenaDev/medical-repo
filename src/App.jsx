feac9d3698708f5bcfa80094a49b677a8e6ed2d9
Aditi Patil <aditiprakashpatil7@gmail.com>
2024-12-31 13:31:46 +0530

    added side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import React, { useState } from 'react';
-import styles from './App.module.scss';
-import Login from './pages/login';
-import PasswordReset from './pages/passwordReset';
-import RecoveryLink from './pages/recoveryLink';
-import UpdatePassword from './pages/updatePassword';
-import Logo from './components/Logo';
-import History from './pages/admin/History';
+import React, { useState } from "react";
+import styles from "./App.module.scss";
+import Login from "./pages/login";
+import PasswordReset from "./pages/passwordReset";
+import RecoveryLink from "./pages/recoveryLink";
+import UpdatePassword from "./pages/updatePassword";
+import Logo from "./components/Logo";
+import History from "./pages/admin/History";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import styles2 from "./App2.module.scss";
 
 function App() {
-
   return (
     <>
-      {/* <Logo/> */}
-      {/* <History/> */}
-        
+      <div className={styles2.main}>
+        <Router>
+
+          <div className={styles2.left}>
+            <Logo />
+            <Sidebar role="admin" />
+          </div>
+
+
+
+          <div className={styles2.right}>
+            
+
+
+          </div>
+        </Router>
+      </div>
     </>
   );
 }
