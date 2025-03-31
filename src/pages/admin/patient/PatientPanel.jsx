1fe7ff70dae0b4b4a80383e1a6be42cb97821854
Yash Bhatt <30y.bhatt@gmail.com>
2025-03-31 21:42:55 +0530

    Remove margin in patient header

--- src/pages/admin/patient/PatientPanel.jsx
+++ src/pages/admin/patient/PatientPanel.jsx
@@ -168,7 +168,6 @@ const PatientPanel = (props) => {
               borderTop: "0.5px solid #4A4A4A8C",
               borderBottom: "0.5px solid #4A4A4A8C",
               paddingY: 1.5,
-              marginBottom: 3,
             }}
           >
             <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
