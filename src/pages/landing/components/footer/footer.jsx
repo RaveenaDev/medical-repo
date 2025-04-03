9f1efdc12ab108ccdf48a5478d0fdc32cf2598fd
Yash Bhatt <30y.bhatt@gmail.com>
2025-04-03 15:35:30 +0530

    ADD: responsive footer

--- src/pages/landing/components/footer/footer.jsx
+++ src/pages/landing/components/footer/footer.jsx
@@ -6,45 +6,43 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer__content">
-        <div className="footer__content-block">
-          <div className="footer__column">
-            <div className="heading">Company</div>
-            <div>Home</div>
-            <div>About</div>
-            <div>Products</div>
-            <div>News</div>
-          </div>
-          <div className="footer__column">
-            <div className="heading">Help</div>
-            <div>Customer Support</div>
-            <div>Delivery Details</div>
-            <div>Terms & Conditions</div>
-            <div>Privacy Policy</div>
-          </div>
+        <div className="footer__column">
+          <div className="heading">Company</div>
+          <div>Home</div>
+          <div>About</div>
+          <div>Products</div>
+          <div>News</div>
         </div>
-        <div className="footer__content-block">
-          <div className="footer__column">
-            <div className="heading">Resources</div>
-            <div> Blogs</div>
-            <div>Youtube Channel</div>
-          </div>
-          <div className="footer__column">
-            <div className="heading">Get in touch</div>
-            <div className="address">
-              {" "}
-              SRD House, LNB Road, Mangaldoi, Assam,784125
-            </div>
+        <div className="footer__column">
+          <div className="heading">Help</div>
+          <div>Customer Support</div>
+          <div>Delivery Details</div>
+          <div>Terms & Conditions</div>
+          <div>Privacy Policy</div>
+        </div>
 
-            <div className="address">Email us : hello@organictea.com</div>
-
-            <div className="address">Call us: +91 8210687508</div>
+        <div className="footer__column ">
+          <div className="heading">Resources</div>
+          <div> Blogs</div>
+          <div>Youtube Channel</div>
+        </div>
+        <div className="footer__column address-col">
+          <div className="heading">Get in touch</div>
+          <div className="address">
+            {" "}
+            SRD House, LNB Road, Mangaldoi, Assam,784125
           </div>
+
+          <div className="address">Email us : hello@organictea.com</div>
+
+          <div className="address">Call us: +91 8210687508</div>
         </div>
       </div>
       <div className="footer__divider"></div>
       <div className="footer__bottom">
         <div className="copyright-info">
-          © Copyright 2024, All Rights Reserved by NaturaLeaf
+          <p> © Copyright 2024</p>
+          <p> All Rights Reserved by NaturaLeaf</p>
         </div>
         <div className="footer__icons">
           <a
@@ -52,28 +50,28 @@ const Footer = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaTwitter />
+            <FaTwitter className="icon" />
           </a>
           <a
             href="https://facebook.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaFacebook />
+            <FaFacebook className="icon" />
           </a>
           <a
             href="https://instagram.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaInstagram />
+            <FaInstagram className="icon" />
           </a>
           <a
             href="https://github.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaGithub />
+            <FaGithub className="icon" />
           </a>
         </div>
       </div>
