bd32b89165c98e4b732b1d741cb424eb7e4301fc
Shrey Vichare <smashshrey@gmail.com>
2025-06-06 16:46:26 +0530

    UPDATE Consultation

--- src/pages/doctor/consultation/components/PatientNewForm.jsx
+++ src/pages/doctor/consultation/components/PatientNewForm.jsx
@@ -1,5 +1,5 @@
 import styles from "./PatientNewForm.module.scss";
-import { ChevronLeft, ChevronUp, ChevronDown } from "lucide-react";
+import { ChevronLeft, SquarePen } from "lucide-react";
 import { useState } from "react";
 
 const PatientNewForm = ({ onBack }) => {
@@ -31,9 +31,106 @@ const PatientNewForm = ({ onBack }) => {
               <p className={styles.pType}>patient type</p>
             </div>
           </div>
+
+          {/* row 3 */}
+          <div className={styles.row3}>
+            <p>Pre Definable Fields</p>
+          </div>
+
+          {/* row 4 */}
+          <div className={styles.row4}>
+            <p>Symptoms</p>
+            <SquarePen size={21} />
+          </div>
+
+          {/* row 5 */}
+          <div className={styles.row5}>
+            <p>Cold, Fever, Headache</p>
+          </div>
+
+          {/* row 6 */}
+          <div className={styles.row4}>
+            <p>Diagnosis</p>
+            <SquarePen size={21} />
+          </div>
+
+          {/* row 7 */}
+          <div className={styles.row5}>
+            <p>Common Cold</p>
+          </div>
         </div>
-        <div className={styles.middlePanel}></div>
-        <div className={styles.rightPanel}></div>
+
+        {/* Middle Panel */}
+        <div className={styles.middlePanel}>
+          {/* Section 1*/}
+          <div className={styles.section1}>
+            {/* row1 */}
+            <div className={styles.mRow1}>
+              <p>Form Title</p>
+              <input type="text" className={styles.input1} />
+            </div>
+
+            {/* row2 */}
+            <div className={styles.mRow2}>
+              <p className={styles.question}>Sample question</p>
+              <input
+                type="text"
+                placeholder="Answer Text Holder"
+                className={styles.input1}
+              />
+            </div>
+          </div>
+
+          {/* Section 2*/}
+          <div className={styles.section2}>
+            {/* guide text */}
+            <div className={styles.guideText}>
+              <p>
+                Drag fields here or
+                <br /> click to add new
+              </p>
+            </div>
+          </div>
+
+          {/* Section 3*/}
+          <div className={styles.section3}>
+            {/* last row */}
+            <div className={styles.mLastRow}>
+              <button className={styles.cancelBtn}>Cancel</button>
+              <button className={styles.saveBtn}>Save Form</button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.rightPanel}>
+          {/* row 1*/}
+          <div className={styles.rRow1}>
+            <p>Add Custom Fields</p>
+          </div>
+
+          {/* Right Panel Buttons*/}
+          <div className={styles.rightPanelButtons}>
+            {/* row 2 */}
+            <button className={styles.rRow2}>
+              <img src="/assets/dropDownIcon.svg" alt="" />
+              <p>Dropdown Menu</p>
+            </button>
+            {/* row 3 */}
+            <button className={styles.rRow2}>
+              <img src="/assets/checklistIcon.svg" alt="" />
+              <p>Checklist</p>
+            </button>
+            {/* row 4 */}
+            <button className={styles.rRow2}>
+              <img src="/assets/multilineIcon.svg" alt="" />
+              <p>Multiline Text Field</p>
+            </button>
+            {/* row 5 */}
+            <button className={styles.rRow2}>
+              <img src="/assets/uploadIcon.svg" alt="" />
+              <p>Multiline Text Field</p>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
