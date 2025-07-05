ace2769f6473ef89b8102583b027ee0eb2a6f85e
Yash Bhatt <30y.bhatt@gmail.com>
2025-07-05 22:52:40 +0530

    Inventory Ui Done

--- src/pages/doctor/inventory/Inventory.jsx
+++ src/pages/doctor/inventory/Inventory.jsx
@@ -495,12 +495,27 @@ const Inventory = () => {
           Category: <span>PPE Kits</span>
         </div>
         <div className={inventoryStyles.actions}>
-          <input
-            className={inventoryStyles.search}
-            placeholder="Search"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
+          <div className={inventoryStyles.searchWrapper}>
+            <svg
+              className={inventoryStyles.searchIcon}
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 32 32"
+            >
+              <path
+                fill="currentColor"
+                d="M26.1333 28L17.7333 19.6C17.0667 20.1333 16.3 20.5556 15.4333 20.8667C14.5667 21.1778 13.6444 21.3333 12.6667 21.3333C10.2444 21.3333 8.19467 20.4942 6.51733 18.816C4.84 17.1378 4.00089 15.088 4 12.6667C3.99911 10.2453 4.83822 8.19556 6.51733 6.51733C8.19645 4.83911 10.2462 4 12.6667 4C15.0871 4 17.1373 4.83911 18.8173 6.51733C20.4973 8.19556 21.336 10.2453 21.3333 12.6667C21.3333 13.6444 21.1778 14.5667 20.8667 15.4333C20.5556 16.3 20.1333 17.0667 19.6 17.7333L28 26.1333L26.1333 28ZM12.6667 18.6667C14.3333 18.6667 15.7502 18.0836 16.9173 16.9173C18.0844 15.7511 18.6676 14.3342 18.6667 12.6667C18.6658 10.9991 18.0827 9.58267 16.9173 8.41733C15.752 7.252 14.3351 6.66844 12.6667 6.66667C10.9982 6.66489 9.58178 7.24844 8.41733 8.41733C7.25289 9.58622 6.66933 11.0027 6.66667 12.6667C6.664 14.3307 7.24756 15.7476 8.41733 16.9173C9.58711 18.0871 11.0036 18.6702 12.6667 18.6667Z"
+              />
+            </svg>
+            <input
+              className={inventoryStyles.searchInput}
+              placeholder="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+
           <button
             className={inventoryStyles.addBtn}
             onClick={() => setShowModal(true)}
@@ -511,47 +526,43 @@ const Inventory = () => {
       </div>
 
       <div className={inventoryStyles.tableContainer}>
-        <table className={inventoryStyles.table}>
-          <thead>
-            <tr>
-              <th>Item name</th>
-              <th>Quantity</th>
-              <th>Usage</th>
-              <th>Status</th>
-              <th>Last Updated</th>
-            </tr>
-          </thead>
-          <tbody>
-            {categoryData[selectedCategory]
-              .filter((item) => {
-                const lower = searchTerm.toLowerCase();
-                return (
-                  item.name.toLowerCase().includes(lower) ||
-                  item.status.toLowerCase().includes(lower) ||
-                  item.date.toLowerCase().includes(lower)
-                );
-              })
-              .map((item, i) => (
-                <tr key={i}>
-                  <td>{item.name}</td>
-                  <td>{item.quantity}</td>
-                  <td>{item.usage}</td>
-                  <td
-                    className={
-                      item.status === "Low Stock"
-                        ? inventoryStyles.low
-                        : item.status === "Moderate"
-                        ? inventoryStyles.moderate
-                        : inventoryStyles.sufficient
-                    }
-                  >
-                    {item.status}
-                  </td>
-                  <td>{item.date}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
+        <div className={inventoryStyles.tableHeader}>
+          <span>Item Name</span>
+          <span>Quantity</span>
+          <span>Usage</span>
+          <span>Status</span>
+          <span>Last Updated</span>
+        </div>
+
+        <div className={inventoryStyles.tableBody}>
+          {categoryData[selectedCategory]
+            .filter((item) => {
+              const lower = searchTerm.toLowerCase();
+              return (
+                item.name.toLowerCase().includes(lower) ||
+                item.status.toLowerCase().includes(lower) ||
+                item.date.toLowerCase().includes(lower)
+              );
+            })
+            .map((item, i) => (
+              <div key={i} className={inventoryStyles.tableRow}>
+                <span>{item.name}</span>
+                <span>{item.quantity}</span>
+                <span>{item.usage}</span>
+                <span
+                  className={
+                    inventoryStyles[
+                      item.status.replace(/\s/g, "").toLowerCase()
+                    ]
+                  }
+                >
+                  {item.status}
+                </span>
+
+                <span>{item.date}</span>
+              </div>
+            ))}
+        </div>
       </div>
 
       {showModal && <AddItemModal onClose={() => setShowModal(false)} />}
