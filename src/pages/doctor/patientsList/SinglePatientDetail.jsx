4bb827e50c082ce7509eeb9f760bda4891af8f36
Shrey Vichare <smashshrey@gmail.com>
2025-07-08 17:26:36 +0530

    Single Patient Details Done

--- src/pages/doctor/patientsList/SinglePatientDetail.jsx
+++ src/pages/doctor/patientsList/SinglePatientDetail.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import CommonPanelMini from "../components/CommonPanelMini";
 import styles from "./SinglePatientDetail.module.scss";
-import { Plus } from "lucide-react";
+import { Plus, ChevronLeft } from "lucide-react";
 import PatientProfile from "./component/PatientProfile";
 import PatientPreviousRecord from "./component/records/PatientPreviousRecord.jsx";
 import { useNavigate } from "react-router-dom";
 const SinglePatientDetail = () => {
   const [activeTab, setActiveTab] = useState("profile");
   const navigate = useNavigate();
-
+  const handleBackBtn = () => {
+    navigate("/doctor/patientList");
+  };
   const handleUpdateDiagnosis = () => {
     navigate("/doctor/consultation");
   };
@@ -18,6 +20,13 @@ const SinglePatientDetail = () => {
       <div className={styles.container}>
         <header className={styles.header}>
           <div className={styles.headerLeft}>
+            <div className={styles.backBtnWrapper}>
+              <ChevronLeft
+                size={25}
+                className={styles.backIcon}
+                onClick={handleBackBtn}
+              />
+            </div>
             <div
               onClick={() => setActiveTab("profile")}
               className={
@@ -41,7 +50,7 @@ const SinglePatientDetail = () => {
           </div>
           <div className={styles.headerRight}>
             <button onClick={handleUpdateDiagnosis}>
-              <Plus size={20} />
+              <Plus className={styles.plusIcon} />
               Update Diagnosis
             </button>
           </div>
