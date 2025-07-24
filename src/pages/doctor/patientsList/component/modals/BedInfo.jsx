40ea0b9c882ce108c184fc39981f5115a892d703
Shrey Vichare <smashshrey@gmail.com>
2025-07-24 13:20:34 +0530

    fix bed

--- src/pages/doctor/patientsList/component/modals/BedInfo.jsx
+++ src/pages/doctor/patientsList/component/modals/BedInfo.jsx
@@ -276,17 +276,12 @@ const BedInfo = ({ onClose, patientId }) => {
                         )}
                       </div>
                     </div>
-                    <div className={styles.changeBed}>
-                      <p className={styles.sectionHeadingS}>
-                        Change Bed No. To
-                      </p>
-                      <input type="text" className={styles.inputText} />
-                    </div>
+
                     <div className={styles.changeReason}>
                       <p className={styles.sectionHeadingS}>
                         Reason For Change
                       </p>
-                      <textarea name="" rows={3} id=""></textarea>
+                      <textarea name="" rows={4} id=""></textarea>
                     </div>
                   </div>
                 </div>
