aaedde2b3d984d540ce12fb642d08ad4d33f9b61
Shrey Vichare <smashshrey@gmail.com>
2025-08-08 15:51:03 +0530

    ADD TPA

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -17,6 +17,7 @@ const roleOptions = {
     { title: "Overview", path: "/admin" },
     { title: "Reception", path: "/admin/reception" },
     { title: "Departments", path: "/admin/departments" },
+    { title: "TPA", path: "/admin/tpa" },
     { title: "Expenses", path: "/admin/expenses" },
     { title: "Settings", path: "/admin/settings" },
   ],
