bd153764eb48a3970d1210688e1bf5c26333d640
Shrey Vichare <smashshrey@gmail.com>
2025-08-18 14:17:05 +0530

    ADD Loader in doc billing

--- src/pages/doctor/patientsList/component/components/BillingDetails.jsx
+++ src/pages/doctor/patientsList/component/components/BillingDetails.jsx
@@ -3,20 +3,26 @@ import styles from "./BillingDetails.module.scss";
 import { ChevronLeft } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 import { getBillsByPatientId } from "../../../../../components/State/Doctor/Action";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Box } from "@mui/material";
 const BillingDetails = ({ onClose }) => {
   const location = useLocation();
   const patientId = location.state?.patientId;
   const patientName = location.state?.patientName;
+  const [loading, setLoading] = useState(true);
 
   //console.log("Patient Id", patientId);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (location.state?.patientId) {
-      dispatch(getBillsByPatientId(location.state.patientId));
+      setLoading(true);
+      dispatch(getBillsByPatientId(location.state.patientId)).finally(() =>
+        setLoading(false)
+      );
     }
-  }, [patientId, location.state]);
+  }, [patientId, location.state, dispatch]);
 
   const patientBills = useSelector((state) => state.doctor.patientBills || []);
   // console.log("Patient Bills", patientBills);
@@ -48,175 +54,189 @@ const BillingDetails = ({ onClose }) => {
           Billing Details: <span>{patientName || "N/A"}</span>
         </p>
       </header>
+      {loading ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "36vh", // or full height you need
+          }}
+        >
+          <CircularProgress sx={{ color: "#25307F" }} size={58} />
+        </Box>
+      ) : (
+        <div>
+          {/* Section 1 */}
+          <div className={styles.section1}>
+            <div className={styles.s1row1}>
+              <div className={styles.s1row1Child}>
+                <p className={styles.label}>Invoice Number</p>
+                <p className={styles.value}>{invoiceNumber || "N/A"}</p>
+              </div>
+              <div className={styles.s1row1Child}>
+                <p className={styles.label}>Invoice Date</p>
+                <p className={styles.value}>
+                  {invoiceDate
+                    ? new Date(invoiceDate).toLocaleDateString("en-IN", {
+                        day: "2-digit",
+                        month: "2-digit",
+                        year: "numeric",
+                      })
+                    : "N/A"}
+                </p>
+              </div>
+            </div>
 
-      {/* Section 1 */}
-      <div className={styles.section1}>
-        <div className={styles.s1row1}>
-          <div className={styles.s1row1Child}>
-            <p className={styles.label}>Invoice Number</p>
-            <p className={styles.value}>{invoiceNumber || "N/A"}</p>
-          </div>
-          <div className={styles.s1row1Child}>
-            <p className={styles.label}>Invoice Date</p>
-            <p className={styles.value}>
-              {invoiceDate
-                ? new Date(invoiceDate).toLocaleDateString("en-IN", {
-                    day: "2-digit",
-                    month: "2-digit",
-                    year: "numeric",
-                  })
-                : "N/A"}
-            </p>
-          </div>
-        </div>
+            <div className={styles.s1row2}>
+              <div className={styles.s1row2Child}>
+                <p className={styles.label}>Description</p>
+                {safeServices.length > 0 ? (
+                  safeServices.map((service, index) => (
+                    <div key={index}>
+                      {(Array.isArray(service.categories)
+                        ? service.categories
+                        : []
+                      ).map((category, idx) => (
+                        <p key={idx} className={styles.value}>
+                          {category.subCategoryName || "N/A"}
+                        </p>
+                      ))}
+                    </div>
+                  ))
+                ) : (
+                  <p className={styles.value}>N/A</p>
+                )}
+              </div>
+              <div className={styles.s1row2Child}>
+                <p className={`${styles.label2} `}>Quantity</p>
+                {safeServices.length > 0 ? (
+                  safeServices.map((service, index) => (
+                    <div key={index}>
+                      {(Array.isArray(service.categories)
+                        ? service.categories
+                        : []
+                      ).map((category, idx) => (
+                        <p key={idx} className={styles.value2}>
+                          {category.quantity || "N/A"}
+                        </p>
+                      ))}
+                    </div>
+                  ))
+                ) : (
+                  <p className={styles.value2}>N/A</p>
+                )}
+              </div>
+              <div className={styles.s1row2Child}>
+                <p className={`${styles.label2} `}>Price</p>
+                {safeServices.length > 0 ? (
+                  safeServices.map((service, index) => (
+                    <div key={index}>
+                      {(Array.isArray(service.categories)
+                        ? service.categories
+                        : []
+                      ).map((category, idx) => (
+                        <p key={idx} className={styles.value2}>
+                          ₹{category.total || "N/A"}
+                        </p>
+                      ))}
+                    </div>
+                  ))
+                ) : (
+                  <p className={styles.value2}>N/A</p>
+                )}
+              </div>
+            </div>
 
-        <div className={styles.s1row2}>
-          <div className={styles.s1row2Child}>
-            <p className={styles.label}>Description</p>
-            {safeServices.length > 0 ? (
-              safeServices.map((service, index) => (
-                <div key={index}>
-                  {(Array.isArray(service.categories)
-                    ? service.categories
-                    : []
-                  ).map((category, idx) => (
-                    <p key={idx} className={styles.value}>
-                      {category.subCategoryName || "N/A"}
-                    </p>
-                  ))}
-                </div>
-              ))
-            ) : (
-              <p className={styles.value}>N/A</p>
-            )}
+            <div className={styles.s1row3}>
+              <div className={styles.s1row3Child}>
+                <p className={styles.label}>Total</p>
+              </div>
+              <div className={styles.s1row3Child}>
+                <p className={styles.label}>₹{totalAmount || "N/A"}</p>
+              </div>
+            </div>
           </div>
-          <div className={styles.s1row2Child}>
-            <p className={`${styles.label2} `}>Quantity</p>
-            {safeServices.length > 0 ? (
-              safeServices.map((service, index) => (
-                <div key={index}>
-                  {(Array.isArray(service.categories)
-                    ? service.categories
-                    : []
-                  ).map((category, idx) => (
-                    <p key={idx} className={styles.value2}>
-                      {category.quantity || "N/A"}
-                    </p>
-                  ))}
-                </div>
-              ))
-            ) : (
-              <p className={styles.value2}>N/A</p>
-            )}
-          </div>
-          <div className={styles.s1row2Child}>
-            <p className={`${styles.label2} `}>Price</p>
-            {safeServices.length > 0 ? (
-              safeServices.map((service, index) => (
-                <div key={index}>
-                  {(Array.isArray(service.categories)
-                    ? service.categories
-                    : []
-                  ).map((category, idx) => (
-                    <p key={idx} className={styles.value2}>
-                      ₹{category.total || "N/A"}
-                    </p>
-                  ))}
-                </div>
-              ))
-            ) : (
-              <p className={styles.value2}>N/A</p>
-            )}
-          </div>
-        </div>
 
-        <div className={styles.s1row3}>
-          <div className={styles.s1row3Child}>
-            <p className={styles.label}>Total</p>
-          </div>
-          <div className={styles.s1row3Child}>
-            <p className={styles.label}>₹{totalAmount || "N/A"}</p>
-          </div>
-        </div>
-      </div>
-
-      {/* Section 2 */}
-      <div className={styles.section2}>
-        {" "}
-        <div className={styles.s2row1}>
-          <div className={styles.s2row1Child}>
-            <p className={styles.label}>Total Amount</p>
-            <p className={styles.value}>₹{totalAmount || "N/A"}</p>
-          </div>
-          <div className={styles.s2row1Child}>
+          {/* Section 2 */}
+          <div className={styles.section2}>
             {" "}
-            <p className={`${styles.label2} `}>Paid</p>
-            <p className={`${styles.value2}`}>
-              ₹
-              {paidAmount !== undefined && paidAmount !== null
-                ? paidAmount
-                : "N/A"}
-            </p>
+            <div className={styles.s2row1}>
+              <div className={styles.s2row1Child}>
+                <p className={styles.label}>Total Amount</p>
+                <p className={styles.value}>₹{totalAmount || "N/A"}</p>
+              </div>
+              <div className={styles.s2row1Child}>
+                {" "}
+                <p className={`${styles.label2} `}>Paid</p>
+                <p className={`${styles.value2}`}>
+                  ₹
+                  {paidAmount !== undefined && paidAmount !== null
+                    ? paidAmount
+                    : "N/A"}
+                </p>
+              </div>
+              <div className={styles.s2row1Child}>
+                {" "}
+                <p className={`${styles.label2} `}>Outstanding</p>
+                <p className={`${styles.value2}`}>
+                  ₹
+                  {outstanding !== undefined && outstanding !== null
+                    ? outstanding
+                    : "N/A"}
+                </p>
+              </div>{" "}
+              <div className={styles.s2row1Child}>
+                {" "}
+                <p className={`${styles.label2} `}>Status</p>
+                <p
+                  className={`${styles.status} ${
+                    status === "Paid"
+                      ? styles.paid
+                      : status === "Pending"
+                      ? styles.pending
+                      : styles.unknown
+                  }`}
+                >
+                  {status || "N/A"}
+                </p>
+              </div>
+            </div>
+            <div className={styles.s2row2}>
+              <div className={styles.s2row2Child}>
+                <p className={styles.label}>Payment History</p>
+              </div>
+              <div className={styles.s2row2Child}>
+                <p className={styles.value}>
+                  Amount Paid: ₹{" "}
+                  {paidAmount !== undefined && paidAmount !== null
+                    ? paidAmount
+                    : "N/A"}
+                </p>
+                <p className={styles.value}>Mode: {mode || "N/A"}</p>
+                <p className={styles.value}>
+                  Date:{" "}
+                  {invoiceDate
+                    ? new Date(invoiceDate).toLocaleDateString("en-IN", {
+                        day: "2-digit",
+                        month: "2-digit",
+                        year: "numeric",
+                      })
+                    : "N/A"}
+                </p>
+              </div>
+            </div>
           </div>
-          <div className={styles.s2row1Child}>
-            {" "}
-            <p className={`${styles.label2} `}>Outstanding</p>
-            <p className={`${styles.value2}`}>
-              ₹
-              {outstanding !== undefined && outstanding !== null
-                ? outstanding
-                : "N/A"}
-            </p>
-          </div>{" "}
-          <div className={styles.s2row1Child}>
-            {" "}
-            <p className={`${styles.label2} `}>Status</p>
-            <p
-              className={`${styles.status} ${
-                status === "Paid"
-                  ? styles.paid
-                  : status === "Pending"
-                  ? styles.pending
-                  : styles.unknown
-              }`}
-            >
-              {status || "N/A"}
-            </p>
-          </div>
-        </div>
-        <div className={styles.s2row2}>
-          <div className={styles.s2row2Child}>
-            <p className={styles.label}>Payment History</p>
-          </div>
-          <div className={styles.s2row2Child}>
-            <p className={styles.value}>
-              Amount Paid: ₹{" "}
-              {paidAmount !== undefined && paidAmount !== null
-                ? paidAmount
-                : "N/A"}
-            </p>
-            <p className={styles.value}>Mode: {mode || "N/A"}</p>
-            <p className={styles.value}>
-              Date:{" "}
-              {invoiceDate
-                ? new Date(invoiceDate).toLocaleDateString("en-IN", {
-                    day: "2-digit",
-                    month: "2-digit",
-                    year: "numeric",
-                  })
-                : "N/A"}
-            </p>
-          </div>
-        </div>
-      </div>
 
-      {/* Submit Container */}
-      <div className={styles.submitContainer}>
-        <button>
-          <img src="/assets/printWhite.svg" alt="" />
-          Print Bill
-        </button>
-      </div>
+          {/* Submit Container */}
+          <div className={styles.submitContainer}>
+            <button>
+              <img src="/assets/printWhite.svg" alt="" />
+              Print Bill
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
