e8cd02963641179b176ab423dda3eab4725c63a9
Yash Bhatt <30y.bhatt@gmail.com>
2025-11-14 15:58:26 +0530

    feat: Add AI image formatting functionality and enhance whiteboard features in DiagnosisAndVitals

--- src/pages/doctor/consultation/components/ConsultBody.jsx
+++ src/pages/doctor/consultation/components/ConsultBody.jsx
@@ -87,8 +87,10 @@ const ConsultBody = ({
     (app) => app.status === "Ongoing"
   );
 
+  console.log("Ongoing Appointment: ", ongoingAppointment);
+
   const openNextAppointment = (data) => {
-    // console.log("Working")
+    console.log("Working");
     setActiveModal("nextAppointment");
     setIsAllowed(data);
   };
@@ -108,8 +110,8 @@ const ConsultBody = ({
   }, [dispatch, ongoingAppointment]);
 
   const patientDetails = useSelector((store) => store.doctor.patientDetails);
-  console.log(generatedPrescriptionsWithAI);
-  console.log("Prescrition here", generatedNewPrescriptionsWithAI);
+  // console.log(generatedPrescriptionsWithAI);
+  // console.log("Prescrition here", generatedNewPrescriptionsWithAI);
 
   useEffect(() => {
     if (
@@ -160,7 +162,7 @@ const ConsultBody = ({
           )
         : null;
 
-    // console.log("Next Appointment:", nextAppointment);
+    console.log("Next Appointment:", nextAppointment);
     return (
       <>
         <div
@@ -399,7 +401,7 @@ const ConsultBody = ({
             </div>
           </div>
 
-          { !selectedForm ? (
+          {!selectedForm ? (
             <>
               <div
                 className={`${styles["lp-2"]} ${
