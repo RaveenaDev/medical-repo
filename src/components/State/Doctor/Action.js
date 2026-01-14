228b887eca4c05f991e49bbb61eec4544b9f6788
Yash Bhatt <30y.bhatt@gmail.com>
2026-01-14 16:28:58 +0530

    Add BillingDetails module modal and invoice components

--- src/components/State/Doctor/Action.js
+++ src/components/State/Doctor/Action.js
@@ -2054,7 +2054,7 @@ export const formatImageWithAI = async (formData) => {
 
 // BILLING
 export const getBillDetails = (billId) => async (dispatch) => {
-  console.log("Fetching details for bill ID:", billId);
+  // console.log("Fetching details for bill ID:", billId);
 
   try {
     const token = localStorage.getItem("jwt");
@@ -2064,7 +2064,7 @@ export const getBillDetails = (billId) => async (dispatch) => {
         Authorization: `Bearer ${token}`, // Includes the token in the authorization header
       },
     });
-    console.log("Bill Details: ", data);
+    // console.log("Bill Details: ", data);
 
     dispatch({ type: GET_BILL_DETAILS, payload: data });
   } catch (error) {
